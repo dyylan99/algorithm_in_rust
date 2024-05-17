e05886ab43189c5957df776cd6fe17aeb11ecea9
dyylan99 <1975131479@qq.com>
2024-05-17 19:18:44 +0800

    算法

--- bit/src/is_unique.rs
+++ bit/src/is_unique.rs
@@ -1,5 +1,6 @@
 pub fn is_unique(astr: String) -> bool {
     let mut mask = 0;
+    //利用位运算，将字符串中的字符映射到一个32位的整数上
     for c in astr.chars() {
         let idx = c as u8 - 'a' as u8;
         if mask & (1 << idx) != 0 {
